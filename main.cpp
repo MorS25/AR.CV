1766b6cfd64740c76c1e8d865a83baa0949bf3ac
Reese Schultz <reesington@users.noreply.github.com>
2015-04-15 13:38:32 -0500

    Revise some comments

--- main.cpp
+++ main.cpp
@@ -91,13 +91,12 @@ int main(int argc, char** argv) {
         vector<Point2f> scene;
 
         for (int i = 0; i < good_matches.size(); ++i) {
-            //-- Get the keypoints from the good matches
             obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
             scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt ); }
 
         Mat H = findHomography(obj, scene, CV_RANSAC);
 
-        // Get the corners from the image_1 (the object to be "detected"):
+        // Get the corners from the detected object in the scene:
         
         /*  NOTE: All following corners are indexed like so:
    
@@ -117,7 +116,7 @@ int main(int argc, char** argv) {
         // Transform corners based on perspective:
         perspectiveTransform(obj_corners, scene_corners, H);
 
-        // Draw lines between the corners (the mapped object in the scene - image_2):
+        // Draw lines between the corners:
         line(img_matches, scene_corners[0] + Point2f(img_object.cols, 0), scene_corners[1] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
         line(img_matches, scene_corners[1] + Point2f(img_object.cols, 0), scene_corners[2] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
         line(img_matches, scene_corners[2] + Point2f(img_object.cols, 0), scene_corners[3] + Point2f(img_object.cols, 0), Scalar(0, 255, 0), 4);
